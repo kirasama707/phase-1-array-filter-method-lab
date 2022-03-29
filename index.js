f5c77aa760ef906807ca95ebb4265dc904203258
christian <vagetto707@aol.com>
2022-03-29 13:47:12 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(Drivers, string) {
+    const foundMatch = Drivers.filter((drivers) => {
+        if (string.toUpperCase() === drivers.toUpperCase()) {
+            return true
+        }
+    })
+    return foundMatch
+}
+const fuzzyMatch = (arr, string) => {
+   const foundMatch = arr.filter((letters) => {
+    if (string.charAt(0, 2) === letters.charAt(0, 2)) {
+        return true 
+    }
+   })
+   return foundMatch
+}
+function matchName(driver, string) {
+    return driver.filter(driver => driver.name.toLowerCase() === string.toLowerCase())
+}
